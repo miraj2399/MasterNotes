b10b7607012473ed2aaa0fe02712b9bc4a5f7155
Miraj Hossain <miraj2399@gmail.com>
2023-09-23 10:08:38 -0400

    Implemented password hashing using bcrypt

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,18 +1,42 @@
+const bcrypt = require('bcrypt');
 const User = require('../models/UserModel');
 async function SignUpHandler(req, res){
     const {email, password, firstName, lastName} = req.body;
-    console.log("REQ BODY: " + JSON.stringify(req.body));
+    const hasedPassword = await bcrypt.hash(password, 10);
     try {
-        const user = await User.create({email, password, firstName, lastName});
+        const user = await User.create(
+            {
+                email: email,
+                password: hasedPassword,
+                firstName: firstName,
+                lastName: lastName
+            }
+        )
         res.status(201).json(user);
     }
     catch (err) {
-        res.status(400).json({message: err.message});
+        res.status(500).json({message: err.message});
     }
 }
 
 async function LoginHandler(req, res){
-    res.send('LoginHandler');
+    const {email, password} = req.body;
+    const user = await User.findOne({email: email});
+    if (!user) {
+        return res.status(404).json({message: "User not found"});
+    }
+    try {
+        if (await bcrypt.compare(password, user.password)) {
+            res.status(200).json({token:"1234567890"})
+        }
+        else {
+            res.status(401).json({message: "Unauthorized"});
+        }
+    }
+    catch (err) {
+        res.status(500).json({message: err.message});
+    }
+
 }
 
 module.exports = {
