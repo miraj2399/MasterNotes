3b74fbb534bcb42b563d04354ec66f67678659fd
Miraj Hossain <miraj2399@gmail.com>
2023-10-10 12:01:52 -0400

    Corrected the default localhost url

--- client/src/services/AxiosInstance.js
+++ client/src/services/AxiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const AxiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || "http://localhost:4000/api",
+    baseURL: process.env.REACT_APP_API_URL || "http://localhost:4000",
     headers: {
         "Content-Type": "application/json",
     },
