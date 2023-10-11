66676f86c17f337ec49702c1306c7c03f56dd1a0
Mary Farag <maryhfarag@gmail.com>
2023-10-11 15:45:00 -0400

    style changes to group page
    added modal and smaller changes

--- client/src/pages/Group.js
+++ client/src/pages/Group.js
@@ -6,6 +6,7 @@ import { GetGroupByIdService, LeaveGroupService } from "../services/GroupService
 import { useEffect, useState } from "react";
 import InviteJoinGroup from "../components/InviteJoinGroup";
 import  LectureNotePreview  from "../components/LectureNotePreview";
+import { Link } from "react-router-dom";
 
 export default function Group(){
     const {id} = useParams();
@@ -23,17 +24,19 @@ export default function Group(){
 
     return (
         <div className="grid">
-           <div className="flex justify-center items-center gap-2 mb-10">
-            <Typography variant="h5">{group.name}</Typography>
-            <Typography variant="subtitle1">{group.courseTitle}</Typography>
+            <div className="flex h-72 justify-center items-center gap-2 mb-10 bg-cyan-500 bg-opacity-50">
+            <div class="text-center">
+           <h1 color="blue-gray" className="hover:text-gray-600 text-gray-800 text-4xl text-center font-extralight mb-5">{group.name}</h1>
+           <h1 color="blue-gray" className="hover:text-gray-600 text-gray-800 text-xl text-center font-extralight mb-5">{group.courseTitle}</h1>
+           <InviteJoinGroup group={group}/>
            </div>
-              <div className="flex justify-center items-center gap-2 mb-16">
-              <Button color="green" buttonType="filled" size="regular" rounded={false} block={false} iconOnly={false} ripple="light" onClick={() => {
+           </div>
+              <div className="flex justify-center items-center gap-2 mb-10">
+              <Button className="text-white font-light text-sm bg-green-500 hover:bg-green-600" buttonType="filled" size="regular" rounded={false} block={false} iconOnly={false} ripple="light" onClick={() => {
                 window.location.href = `/createGroupNote/${group._id}`
-            }}>
-                Create Note
-            </Button>
+            }}> Create Note </Button>
             </div>
+
 
         <Timeline className="m-3">
          { dates &&
@@ -41,10 +44,10 @@ export default function Group(){
                 <div key={date.date} className="mb-24">
                 <TimelineItem>
                     <TimelineHeader>
-                        <div className="flex justify-center items-center gap-2 p-3 bg-blue-300 rounded-xl ">
-                        <TimelineIcon color="blue" />
+                        <div className="flex justify-center items-center gap-2 p-3 bg-cyan-500 bg-opacity-50 rounded-xl">
+                        <TimelineIcon color="black" />
                         <CalendarTodayIcon/>
-                        <p className="text-lg font-semibold">{new Date(date.date).toDateString()}</p>
+                        <p className="text-black text-lg font-extralight">{new Date(date.date).toDateString()}</p>
                        
                         </div>
                     </TimelineHeader>
@@ -60,6 +63,7 @@ export default function Group(){
                                 />
                             }
                         })}
+                        {!notes && <div>No notes yet</div>}
                         </div>
                     </TimelineBody>
                     <TimelineConnector/>
@@ -68,14 +72,19 @@ export default function Group(){
             ))
           }
         </Timeline>
-        <InviteJoinGroup group={group}/>
-        <button 
-        className="bg-red-500 hover:bg-red-600 text-white px-2 py-1 rounded-lg text-sm font-semibold w-32"
-        onClick={() => {
-        LeaveGroupService(group._id) 
-        window.location.href = "/dashboard"
-        }}
-        >Leave Group</button>
+        <div class="text-center m-5">
+            <Button 
+            className="text-white font-light text-sm bg-red-500 hover:bg-red-600 mr-3"
+            onClick={() => {
+            LeaveGroupService(group._id) 
+            window.location.href = "/dashboard"
+            }}
+            >Leave Group</Button>
+
+              <Link to="/dashboard"><Button className="text-white font-light text-sm ml-3" color="black">
+                Back
+            </Button></Link>
+        </div>
         </div>
     )
 }
